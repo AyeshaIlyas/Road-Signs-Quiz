631519f5204c12764086fc40be50d7c9eac9f54b
AyeshaIlyas <100631958+AyeshaIlyas@users.noreply.github.com>
2022-10-11 16:29:32 -0400

    Add log messages

--- app/src/main/java/edu/sunyulster/roadsigns/HomeActivity.java
+++ app/src/main/java/edu/sunyulster/roadsigns/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
 
 import edu.sunyulster.roadsigns.databinding.ActivityHomeBinding;
 
@@ -18,18 +19,21 @@ public class HomeActivity extends AppCompatActivity {
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.i("HomeActivity.java", "Start button clicked");
                 String input = binding.input.getText().toString();
                 int questionCount;
                 if (input.strip().length() == 0 || Integer.parseInt(input) <= 0) 
                     questionCount = Integer.parseInt(getResources().getString(R.string.defaultQuestionCount);
                 else 
                     questionCount = Integer.parseInt(input);
-
+                Log.i("HomeActivity.java", "questionCount: " + questionCount);
                 Intent intent = new Intent(HomeActivity.this, QuizActivity.class);
                 intent.putExtra("questionCount", questionCount);
+                Log.i("HomeActivity.java", "Intent created...Starting new activity");
                 HomeActivity.this.startActivity(intent);
             }
         });
         setContentView(binding.getRoot());
+        Log.i("HomeActivity.java", "Layout Inflated");  
     }
 }
